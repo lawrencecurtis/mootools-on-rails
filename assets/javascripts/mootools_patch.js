a3fd6e06817eadeb7a5e1d1f3e4f0bc14e5c5253
Pedro Visintin <pedro.visintin@gmail.com>
2008-09-13 14:49:20 -0300

    new mootools_patch added

--- assets/javascripts/mootools_patch.js
+++ assets/javascripts/mootools_patch.js
@@ -25,6 +25,9 @@ Element.implement({
     inserter(el, this);
     content.stripScripts(true);
   },
+  up: function(expression, index) {
+    this.getParent().getChildren(expression)[index || 0];
+  },
   
   appendTop: function(content) { this.append('top', content); },
   
@@ -34,3 +37,80 @@ Element.implement({
   
   appendBefore: function(content) { this.append('before', content); }  
 });
+
+String.implement({
+  gsub: function(pattern, replacement) {
+  var result = '', source = this, match;
+  replacement = arguments.callee.prepareReplacement(replacement);
+
+  while (source.length > 0) {
+    if (match = source.match(pattern)) {
+      result += source.slice(0, match.index);
+      result += String.interpret(replacement(match));
+      source  = source.slice(match.index + match[0].length);
+    } else {
+      result += source, source = '';
+    }
+  }
+  return result;},
+  interpret: function(value) {
+    return value == null ? '' : String(value);
+  },
+  specialChar: {
+    '\b': '\\b',
+    '\t': '\\t',
+    '\n': '\\n',
+    '\f': '\\f',
+    '\r': '\\r',
+    '\\': '\\\\'
+  }
+});
+
+String.prototype.gsub.prepareReplacement = function(replacement) {
+  
+  if ($type(replacement) == 'function') return replacement;
+  var template = new Template(replacement);
+  return function(match) { return template.evaluate(match) };
+};
+
+
+var Template = new Class({
+  initialize: function(template, pattern) {
+    this.template = template.toString();
+    this.pattern = pattern || Template.Pattern;
+  },
+
+  evaluate: function(object) {
+    
+    return this.template.gsub(this.pattern, function(match) {
+      if (object == null) return '';
+    
+      var before = match[1] || '';
+      if (before == '\\') return match[2];
+    
+      var ctx = object;
+      expr = match[3];
+      var pattern = /^([^.[]+|\[((?:.*?[^\\])?)\])(\.|\[|$)/;
+      match = pattern.exec(expr);
+      if (match == null) return before;
+    
+      while (match != null) {
+        var comp = (match[1].indexOf('[') == 0) ? match[2].replace('\\\\]', ']') : match[1];
+        ctx = ctx[comp];
+        if (null == ctx || '' == match[3]) break;
+        expr = expr.substring('[' == match[3] ? match[1].length : match[0].length);
+        match = pattern.exec(expr);
+      }
+      return before + (ctx == null) ? '' : String(ctx);
+    }.bind(this));
+  }
+});
+
+Template.Pattern = /(^|.|\r|\n)(#\{(.*?)\})/;
+
+//Hash.prototype.toTemplateReplacements = Hash.prototype.toObject;
+
+function testTemplate(template_string,object) {
+  var template = new Template(template_string);
+  return template.evaluate(object);
+};
